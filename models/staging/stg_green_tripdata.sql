27408f2814ed20b083690ee2ddfbabc2873bcdf3
Dave Flynn <analogue40@gmail.com>
2023-02-18 11:23:51 +0800

    remove 100 row limit on staging models

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -42,8 +42,8 @@ where rn = 1
 
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
+--{% if var('is_test_run', default=true) %}
 
-  limit 100
+--  limit 100
 
-{% endif %}
+--{% endif %}
--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -42,9 +42,9 @@ from tripdata
 where rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
+--{% if var('is_test_run', default=true) %}
 
-  limit 100
+--  limit 100
 
-{% endif %}
+--{% endif %}
 
